6a5a471fa8265f54671eac31e412dd785c26bea8
41341115S <41341115s@gapps.ntnu.edu.tw>
2025-12-01 23:35:13 +0800

    branch_sub_int sub

--- main.c
+++ main.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
 
+int sub(int a,int b){
+    return a-b;
+}
+
 int main(){
     printf("Do not hello World");
+
+    int a=10;
+    int b=20;
+    printf("%d",sub(a,b));
 
     return 0;
 }
